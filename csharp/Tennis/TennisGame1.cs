bb66da1aa59dfa477e3853b115ba4e4f2cf0be93
Sara Fadel <sarafadel@clubbertv.com>
2025-12-24 14:52:42 +0200

    improve code readablity by using switch expressions instead of switch

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -4,8 +4,10 @@ namespace Tennis
     {
         private int m_score1 = 0;
         private int m_score2 = 0;
-       private string player1Name;
+        private string player1Name;
         private string player2Name;
+
+
 
         public TennisGame1(string player1Name, string player2Name)
         {
@@ -24,10 +26,10 @@ namespace Tennis
         public string GetScore()
         {
             string score = "";
-           
+
             if (m_score1 == m_score2)
             {
-                score = GetForEvenScore();
+                score = GetForEvenScore(m_score1);
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
@@ -48,21 +50,8 @@ namespace Tennis
             {
                 if (i == 1) tempScore = m_score1;
                 else { score += "-"; tempScore = m_score2; }
-                switch (tempScore)
-                {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
+
+                 score += ScoreForUnderFourTerms(tempScore);
 
             }
             return score;
@@ -71,35 +60,35 @@ namespace Tennis
 
         private string GetAdvantageOrWinScore()
         {
-            string score;
             var minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
-            return score;
+            return GetAdvantageOrWinScoreTerms(minusResult);
         }
 
-        private string GetForEvenScore()
+
+        private string GetForEvenScore(int points) => points switch
         {
-            string score;
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-            }
-            return score;
-        }
+            0 => "Love-All",
+            1 => "Fifteen-All",
+            2 => "Thirty-All",
+            _ => "Deuce"
+        };
+        private string GetAdvantageOrWinScoreTerms(int resultDifference) => resultDifference switch
+        {
+            1 => "Advantage player1",
+            -1 => "Advantage player2",
+            >= 2 => "Win for player1",
+            _ => "Win for player2"
+        };
+
+
+        private string ScoreForUnderFourTerms(int tempScore) => tempScore switch
+        {
+            0 => "Love",
+            1 => "Fifteen",
+            2 => "Thirty",
+            3 => "Forty",
+            _ => ""
+        };
     }
 }
 
